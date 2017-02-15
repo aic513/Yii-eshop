eab2077ae852b34c493432e54739dba4b9b8532a
aic513 <aic513@mail.ru>
2017-02-15 12:58:41 +0300

    Вывод страницы популярных товаров

--- models/Product.php
+++ models/Product.php
@@ -1,22 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aic513
- * Date: 15.02.2017
- * Time: 10:32
- */
-
 namespace app\models;
+use yii\db\ActiveRecord;
 
 
-class Product{
+class Product extends ActiveRecord{
 
     public static function tableName(){    //определяем таблицу в бд
         return 'product';
     }
 
     public function getCategory(){
-        return $this->hasMany(Category::className(), ['id' => 'category_id']);  //определяем тип связи и связующие поля
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
     }
 
 }
