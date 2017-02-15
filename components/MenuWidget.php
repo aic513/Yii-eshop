e2beb63535b8b2d56bd387482ec9ad0b5b30e0e9
aic513 <aic513@mail.ru>
2017-02-15 11:31:45 +0300

    Сделал виджет для меню со сбором данных из бд

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -1,24 +1,54 @@
 <?php
+
 namespace app\components;
-
 use yii\base\Widget;
+use app\models\Category;
 
-class MenuWidget extends Widget
-{
+class MenuWidget extends Widget{
 
     public $tpl;
+    public $data;
+    public $tree;
+    public $menuHtml;
 
-    public function init()
-    {
+    public function init(){
         parent::init();
-        if ($this->tpl === null) {
+        if( $this->tpl === null ){
             $this->tpl = 'menu';
         }
         $this->tpl .= '.php';
     }
 
-    public function run()
-    {
-        $this->tpl;
+    public function run(){
+        $this->data = Category::find()->indexBy('id')->asArray()->all();   //получаем массив массивов
+        $this->tree = $this->getTree();   //формируем дерево массивов
+        $this->menuHtml = $this->getMenuHtml($this->tree);  //собирем меню
+        return $this->menuHtml;
     }
+
+    protected function getTree(){
+        $tree = [];
+        foreach ($this->data as $id=>&$node) {
+            if (!$node['parent_id'])
+                $tree[$id] = &$node;
+            else
+                $this->data[$node['parent_id']]['childs'][$node['id']] = &$node;
+        }
+        return $tree;
+    }
+
+    protected function getMenuHtml($tree){ 
+        $str = '';
+        foreach ($tree as $category) {
+            $str .= $this->catToTemplate($category);
+        }
+        return $str;
+    }
+
+    protected function catToTemplate($category){  //формируем шаблон в буфере
+        ob_start();
+        include __DIR__ . '/menu_tpl/' . $this->tpl;
+        return ob_get_clean();
+    }
+
 }
