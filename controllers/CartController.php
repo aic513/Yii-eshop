a351131aa7277e79b94ae4b28bb24de7238f03a2
aic513 <aic513@mail.ru>
2017-02-17 01:00:24 +0300

    Сохранение заказа и товаров в самом заказе в бд

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -2,8 +2,9 @@
 namespace app\controllers;
 use app\models\Product;
 use app\models\Cart;
+use app\models\Order;
+use app\models\OrderItems;
 use Yii;
-
 /*Array
 (
     [1] => Array
@@ -72,7 +73,38 @@ class CartController extends AppController{
     }
 
     public function actionView(){
-        return $this->render('view');
+        $session = Yii::$app->session;
+        $session->open();
+        $this->setMeta('Корзина');
+        $order = new Order();
+        if( $order->load(Yii::$app->request->post()) ){
+            $order->qty = $session['cart.qty'];
+            $order->sum = $session['cart.sum'];
+            if($order->save()){        //если заказ сохранен
+                $this->saveOrderItems($session['cart'], $order->id);
+                Yii::$app->session->setFlash('success', 'Ваш заказ принят. Менеджер вскоре свяжется с Вами.');
+                $session->remove('cart');
+                $session->remove('cart.qty');
+                $session->remove('cart.sum');
+                return $this->refresh();
+            }else{
+                Yii::$app->session->setFlash('error', 'Ошибка оформления заказа');
+            }
+        }
+        return $this->render('view', compact('session', 'order'));
+    }
+
+    protected function saveOrderItems($items, $order_id){  //получаем id сохраненной записи при оформлении заказа
+        foreach($items as $id => $item){
+            $order_items = new OrderItems();
+            $order_items->order_id = $order_id;     //id заказа
+            $order_items->product_id = $id;     //id товара
+            $order_items->name = $item['name'];   
+            $order_items->price = $item['price'];
+            $order_items->qty_item = $item['qty'];
+            $order_items->sum_item = $item['qty'] * $item['price'];
+            $order_items->save();
+        }
     }
 
 
