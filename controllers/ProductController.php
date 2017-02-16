5f53be1297e21810bf18e9d9a16d131ae39cc37c
aic513 <aic513@mail.ru>
2017-02-16 17:22:39 +0300

    Выброс исключений,если не найдена категория/товар

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -9,6 +9,9 @@ class ProductController extends AppController{
     public function actionView($id){
         $id = Yii::$app->request->get('id');
         $product = Product::findOne($id);  //ленивая загрузка
+        if(empty($product)) {
+            throw new \yii\web\HttpException(404, 'Такого товара нет');
+        }
 //        $product = Product::find()->with('category')->where(['id' => $id])->limit(1)->one();  //жадная загрузка
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
         $this->setMeta('E-SHOPPER | ' . $product->name, $product->keywords, $product->description);
