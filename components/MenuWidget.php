22f06fdbe2c141afbab4dc5c3ffa8e6e19325675
aic513 <aic513@mail.ru>
2017-02-18 12:28:29 +0300

    Управление категориями

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Andrey
+ * Date: 07.05.2016
+ * Time: 10:35
+ */
 
 namespace app\components;
 use yii\base\Widget;
@@ -8,6 +14,7 @@ use Yii;
 class MenuWidget extends Widget{
 
     public $tpl;
+    public $model;
     public $data;
     public $tree;
     public $menuHtml;
@@ -21,16 +28,19 @@ class MenuWidget extends Widget{
     }
 
     public function run(){
-        //получаем кеш
-        $menu = Yii::$app->cache->get('menu');
-        if($menu) return $menu;
+        // get cache
+        if($this->tpl == 'menu.php'){    //кеш будет записываться только в пользовательскую часть
+            $menu = Yii::$app->cache->get('menu');
+            if($menu) return $menu;
+        }
 
-        $this->data = Category::find()->indexBy('id')->asArray()->all();   //получаем массив массивов
-        $this->tree = $this->getTree();   //формируем дерево массивов
-        $this->menuHtml = $this->getMenuHtml($this->tree);  //собирем меню
-
-        // если кеш пустой,то формируем его
-        Yii::$app->cache->set('menu', $this->menuHtml, 60);
+        $this->data = Category::find()->indexBy('id')->asArray()->all();
+        $this->tree = $this->getTree();
+        $this->menuHtml = $this->getMenuHtml($this->tree);
+        // set cache
+        if($this->tpl == 'menu.php'){
+            Yii::$app->cache->set('menu', $this->menuHtml, 60);
+        }
         return $this->menuHtml;
     }
 
@@ -45,15 +55,15 @@ class MenuWidget extends Widget{
         return $tree;
     }
 
-    protected function getMenuHtml($tree){
+    protected function getMenuHtml($tree, $tab = ''){
         $str = '';
         foreach ($tree as $category) {
-            $str .= $this->catToTemplate($category);
+            $str .= $this->catToTemplate($category, $tab);
         }
         return $str;
     }
 
-    protected function catToTemplate($category){  //формируем шаблон в буфере
+    protected function catToTemplate($category, $tab){
         ob_start();
         include __DIR__ . '/menu_tpl/' . $this->tpl;
         return ob_get_clean();
