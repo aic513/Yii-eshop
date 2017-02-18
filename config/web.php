123e3795da100eea8b2aacbe930ca7090c38a093
aic513 <aic513@mail.ru>
2017-02-18 22:30:36 +0300

    Прикрутил визуальный редактор и файловый менеджер

--- config/web.php
+++ config/web.php
@@ -70,6 +70,18 @@ $config = [
         ],
 
     ],
+    'controllerMap' => [
+        'elfinder' => [
+            'class' => 'mihaildev\elfinder\PathController',
+            'access' => ['@'],
+            'root' => [
+                'baseUrl'=>'/web',
+//                'basePath'=>'@webroot',
+                'path' => 'upload/global',
+                'name' => 'Global'
+            ],
+        ]
+    ],
     'params' => $params,
 ];
 
